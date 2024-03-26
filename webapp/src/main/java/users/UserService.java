d2190e805ff3f71457f1a20b29a0a9c85b44748c
EkaterinaBildina <kukresch@gmail.com>
2024-03-26 22:08:25 +0700

    webapp with users registraion

---
+++ webapp/src/main/java/users/UserService.java
@@ -0,0 +1,24 @@
+package users;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> getAllUsers(){
+        userRepository.save(new User(null, "Ekaterina", "ekaterina1@mail.ru"));
+        return userRepository.findAll();
+    }
+
+    public User getUserById(Long id){
+        return userRepository.findById(id);
+    }
+
+
+}
